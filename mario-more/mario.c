526a2824cd3bae8e552b522c965d0bffe71b5534
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-02-26 21:07:21 +0000

    Sat, Feb 26, 2022, 9:07 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -13,7 +13,10 @@ int main(void)
     int count = 0;
     for (int i = 0; i < rows; i++){
         for (int p = 2*rows+1; p >= 0; p--){
-            if(count >= p-rows-1 && p != rows){
+            if(count >= p-rows-1 && p != rows && p > rows){
+                printf("#");
+            }
+            else if (){
                 printf("#");
             }
             else if (p == rows){
