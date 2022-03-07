be4e38ee95fa214c405ed823e5f45189acfcaa09
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-07 12:51:22 +0000

    Mon, Mar 7, 2022, 12:51 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,12 +5,14 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
     printf("%i \n", count_letters(text));
     printf("%i \n", count_words(text));
+    printf("%i \n", count_sentences(text));
 }
 
 int count_letters(string text)
@@ -28,10 +30,23 @@ int count_letters(string text)
 
 int count_words(string text)
 {
-    int count = 0;
+    int count = 1;
     for(int i = 0, n = strlen(text); i < n; i++)
     {
         if(text[i] == ' ')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_sentences(string text)
+{
+    int count = 0;
+    for(int i = 0, n = strlen(text); i < n; i++)
+    {
+        if(text[i] == '.' ^ text[i] == '?' ^ text[i] == '!')
         {
             count++;
         }
