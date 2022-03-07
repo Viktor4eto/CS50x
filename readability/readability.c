675342b219b8d880088118cd5bcbc4593a23cfe1
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-07 21:31:24 +0000

    Mon, Mar 7, 2022, 9:31 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <math.h>
 
 int count_letters(string text);
 int count_words(string text);
@@ -10,10 +11,11 @@ int count_sentences(string text);
 int main(void)
 {
     string text = get_string("Text: ");
-    int index = 
+    double index = 0.0588*(count_letters(text)/count_words(text)*100) - 0.296*(count_sentences(text)/count_words(text)*100) - 15.8;
     printf("%i \n", count_letters(text));
     printf("%i \n", count_words(text));
     printf("%i \n", count_sentences(text));
+    printf("%f \n", round(index));
 }
 
 int count_letters(string text)
