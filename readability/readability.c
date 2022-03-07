43e2646aef469f440fd25d3e4572220a3d8c6ef4
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-07 12:47:42 +0000

    Mon, Mar 7, 2022, 12:47 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -28,7 +28,7 @@ int count_letters(string text)
 
 int count_words(string text)
 {
-    int count = 1;
+    int count = 0;
     for(int i = 0, n = strlen(text); i < n; i++)
     {
         if(text[i] == ' ')
