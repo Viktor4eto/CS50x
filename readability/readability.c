7a59b0a86a6a1be1509a248773ea99ae6f3eac6f
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-07 12:37:05 +0000

    Mon, Mar 7, 2022, 12:37 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,19 +1,20 @@
 #include <cs50.h>
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int count_letters(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
-    printf("%i", count_letters(text));
+    printf("%i \n", count_letters(text));
 }
 
 int count_letters(string text)
 {
-    int count;
-    for(int i = 0; n = strlen(text); i < n; i++)
+    int count = 0;
+    for(int i = 0, n = strlen(text); i < n; i++)
     {
         if(islower(text[i]) ^ isupper(text[i]))
         {
