e9c9454f95bb6ae2108ea369e78c493c739fa145
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-07 08:10:20 +0000

    Mon, Mar 7, 2022, 8:10 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
-{
+int count_letters(string text)
 
+int main(void)
+{
+    string text = get_string("Text: ");
+
+  }
+
+int count_letters(string text)
+{
+    int count;
+    for(int i = 0; n = strlen(text); i < n; i++)
+    {
+        if(islower(text[i]) ^ isupper(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
 }
