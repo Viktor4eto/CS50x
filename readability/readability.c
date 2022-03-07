51ab46bfdcd63cd8ecad81a5ae23451abeb7acab
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-07 21:50:16 +0000

    Mon, Mar 7, 2022, 9:50 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,60 +10,73 @@ int count_sentences(string text);
 
 int main(void)
 {
+    //Gets text
     string text = get_string("Text: ");
+    //Variables for easier understanding of the code
     float letters = count_letters(text);
     float words = count_words(text);
     float sentences = count_sentences(text);
-    float L = letters/words;
-    float S = sentences/words;
-    float index = 0.0588*L*100 - 0.296*S*100 - 15.8;
+    float L = letters / words;
+    float S = sentences / words;
+    //Formula
+    float index = 0.0588 * L * 100 - 0.296 * S * 100 - 15.8;
+    //Grade logic
     if (index < 1)
     {
+        //Before Grade 1
         printf("Before Grade 1 \n");
     }
     else if (index > 16)
     {
+        //Grade 16+
         printf("Grade 16+ \n");
     }
     else
     {
+        //Grade X
         printf("Grade %i \n", (int) round(index));
     }
 
 }
-
+//Counts letters
 int count_letters(string text)
 {
     int count = 0;
-    for(int i = 0, n = strlen(text); i < n; i++)
+    //Loop for counting
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if(islower(text[i]) ^ isupper(text[i]))
+        //Check for letters
+        if (islower(text[i]) ^ isupper(text[i]))
         {
             count++;
         }
     }
     return count;
 }
-
+// Counts words
 int count_words(string text)
 {
     int count = 1;
-    for(int i = 0, n = strlen(text); i < n - 1; i++)
+    //Loop
+    for (int i = 0, n = strlen(text); i < n - 1; i++)
     {
-        if(text[i] == ' ')
+        //Checks for spaces
+        if (text[i] == ' ')
         {
             count++;
         }
     }
     return count;
 }
-
+//Counts sentences
 int count_sentences(string text)
 {
     int count = 0;
-    for(int i = 0, n = strlen(text); i < n; i++)
+    //Loop
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if(text[i] == '.' ^ text[i] == '?' ^ text[i] == '!')
+        //Checks for punctuation
+        if (text[i] == '.' ^ text[i] == '?' ^ text[i] == '!')
         {
             count++;
         }
