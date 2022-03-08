16d4105913ec6246ecb51b15e0fe71d7bb946dcf
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-08 06:51:48 +0000

    Tue, Mar 8, 2022, 6:51 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,24 +2,25 @@
 #include <stdio.h>
 #include <ctype.h>
 
-char substitute(char x);
+char substitute(char x, string key);
 
 int main(int argc, string argv[])
 {
-
+    char c = get_char("C = ");
+    printf("%c \n", substitute(c, argv[1]));
 }
 
-char substitute(char x)
+char substitute(char x, string key)
 {
     int i = (int) x;
     char new;
-    if (i > 64 && i < 91)
+    if (isupper(x))
     {
-        new = argv[1][i - 65]
+        new = key[i - 65];
     }
-    else if (i > 140 && i < 173)
-    {
-        new = argv[1][i - 141]
+    else if (islower(x))
+
+        new = key[i - 141];
     }
     return new;
 }
