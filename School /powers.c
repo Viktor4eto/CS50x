b61b4802292cf07aa73c59c0fba40f50a9157622
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-08 11:14:20 +0000

    Tue, Mar 8, 2022, 11:14 AM +00:00

---
+++ School /powers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int sum = 0;
+    do
+    {
+        int n = get_int("n: ");
+    }
+    while (n < 0);
+
+    for(int i = 0; i > n; i++)
+    {
+        sum +=
+        
+    }
+}
