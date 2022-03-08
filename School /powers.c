1a9a524803ad89ad75762b67895e149bb54dac0d
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-08 11:22:32 +0000

    Tue, Mar 8, 2022, 11:22 AM +00:00

--- School /powers.c
+++ School /powers.c
@@ -11,9 +11,9 @@ int main(void)
         n = get_int("n: ");
     } while (n < 0);
 
-    for(double i = 1.0; i >= n; i++)
+    for(double i = 1.0; i <= n; i++)
     {
-        printf("%f \n", i);
+        //printf("%f \n", i);
         sum += pow(i, i);
     }
     printf("%i \n", sum);
