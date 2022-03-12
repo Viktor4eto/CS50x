c761f11a0c1f910d0be73870bac0c82d639ca7e3
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-12 12:40:19 +0000

    Sat, Mar 12, 2022, 12:40 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,28 +3,30 @@
 #include <ctype.h>
 #include <string.h>
 
-string substitute(string x, string key);
+void substitute(string x, string key);
 
 int main(void)
 {
     string c = get_string("C = ");
     string arg = "JTREKYAVOGDXPSNCUIZLFBMWHQ";
-    printf("%s \n", substitute(c, arg));
+    substitute(c, arg);
 }
 
-string substitute(string x, string key)
+void substitute(string x, string key)
 {
-    string new = "";
     for (int i = 0, n = strlen(x); i < n; i++)
     {
         if (isupper(x[i]))
         {
-            new[i] = key[i] - 65;
+            printf("%c", key[i] - 65);
         }
         else if (islower(x[i]))
         {
-            new[i] = key[i] - 141;
+            printf("%c", key[i] - 141);
+        }
+        else
+        {
+            printf("%c", x[i]);
         }
     }
-    return new;
 }
