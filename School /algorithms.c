3557028986e1bad2b90b78f67e0d590174e90047
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-21 15:43:20 +0000

    Mon, Mar 21, 2022, 3:43 PM +00:00

--- School /algorithms.c
+++ School /algorithms.c
@@ -14,9 +14,17 @@ int main(void)
         n = get_int("n = ");
     } while (n < 0);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <= n; i++)
     {
         S = S + i;
+        printf("%i \n", i);
+    }
+    printf("while: \n");
+    while (n != 0)
+    {
+        S = S + n;
+        n--;
+        printf("%i \n", n);
     }
     printf("n: %i, S = %i \n", n, S);
 }
