458b46be13991325316a8260a8deb26b2dc6115e
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-21 07:28:48 +0000

    Mon, Mar 21, 2022, 7:28 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -103,7 +103,7 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp(name, candidates[i]) == 0)
         {
-            ranks
+            ranks[i] = rank;
         }
     }
     return false;
