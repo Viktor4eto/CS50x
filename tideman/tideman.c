6b93eacc9dab973fbfda8aca782b0a808a40c1e5
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-21 07:32:45 +0000

    Mon, Mar 21, 2022, 7:32 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -104,6 +104,7 @@ bool vote(int rank, string name, int ranks[])
         if (strcmp(name, candidates[i]) == 0)
         {
             ranks[i] = rank;
+            return true;
         }
     }
     return false;
@@ -112,7 +113,13 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i; j , candidate_count; j++)
+        {
+
+        }
+    }
     return;
 }
 
