986c6631e629ed1cae81523086a3695680e7798d
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-22 19:27:11 +0000

    Tue, Mar 22, 2022, 7:27 PM +00:00

---
+++ School/sorter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    int sum = 0;
+    int n;
+    do
+    {
+        n = get_int("n: ");
+    } while (n < 0);
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; )
+    }
+    printf("%i \n", sum);
+}
