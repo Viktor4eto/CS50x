600fd1fbc21b4e9f7ab75aa1c20fa83e6a2ec5a4
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-26 17:42:39 +0000

    Sat, Mar 26, 2022, 5:42 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,5 +1,6 @@
 #include "helpers.h"
 #include <math.h>
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -19,6 +20,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = ;
+            image[i][j].rgbtGreen = ;
+            image[i][j].rgbtRed = ;
+        }
+    }
     return;
 }
 
