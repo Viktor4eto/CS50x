2b7dab8d635d4a1ae18fc5b6c178abb0a87df1c7
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-26 18:11:45 +0000

    Sat, Mar 26, 2022, 6:11 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -9,6 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             int rgb = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3);
+            
             image[i][j].rgbtBlue = rgb;
             image[i][j].rgbtGreen = rgb;
             image[i][j].rgbtRed = rgb;
@@ -22,11 +23,20 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width/2; j++)
         {
+            RGBTRIPLE save;
+            save.rgbtBlue = image[i][j].rgbtBlue;
+            save.rgbtGreen = image[i][j].rgbtGreen;
+            save.rgbtRed = image[i][j].rgbtRed;
+
             image[i][j].rgbtBlue = image[i][width - j].rgbtBlue;
             image[i][j].rgbtGreen = image[i][width - j].rgbtGreen;
             image[i][j].rgbtRed = image[i][width - j].rgbtRed;
+
+            image[i][width - j].rgbtBlue = save.rgbtBlue;
+            image[i][width - j].rgbtGreen = save.rgbtGreen;
+            image[i][width - j].rgbtRed = save.rgbtRed;
         }
     }
     return;
