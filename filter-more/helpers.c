984efb68d3a218ee979cfd2557fbb9e6e603473a
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-26 17:35:34 +0000

    Sat, Mar 26, 2022, 5:35 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,9 +1,18 @@
 #include "helpers.h"
-
+#include <math.h>
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int rgb = round((image[i][j].rdbtBlue + image[i][j].rdbtGreen + image[i][j].rdbtRed)/3);
+            image[i][j].rdbtBlue = rgb;
+            image[i][j].rdbtGreen = rgb;
+            image[i][j].rdbtRed = rgb;
+        }
+    }
     return;
 }
 
