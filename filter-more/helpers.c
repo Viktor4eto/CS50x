962390188bf2a75c644e2c1b767352c2fd007acb
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-03-27 07:02:18 +0000

    Sun, Mar 27, 2022, 7:02 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -9,7 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int rgb = ceil((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3);
+            int rgb = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3;
 
             image[i][j].rgbtBlue = rgb;
             image[i][j].rgbtGreen = rgb;
