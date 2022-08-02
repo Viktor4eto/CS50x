2e2cdf7f01d82bb1ad97bc9a0697dd7a9aabd49f
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-08-02 12:23:10 +0000

    Tue, Aug 2, 2022, 12:23 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -130,7 +130,7 @@ void add_pairs(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j =0; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             if (preferences[i][j] > preferences[j][i])
             {
