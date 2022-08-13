4a263359995c00dc8a1a627efb95267d7ac4adb8
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2022-08-13 11:58:56 +0000

    ddd, Aug 8/13/2022, YYYY, 11:58 A +0

--- recover/recover.c
+++ recover/recover.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 
                 if (JPEG_counter == 0)
                 {
-                    
+
                 }
                 if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
                 {
