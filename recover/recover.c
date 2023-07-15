f5d49de11d2e8964a0ab0e5ba08c2f973893d45c
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-07-15 13:52:15 +0000

    Sat, Jul 15, 2023, 4:52 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,17 +7,19 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
+    //Checks if there is an inputted image
     if (argv[1] == NULL)
     {
         printf("Usage: ./recover IMAGE \n");
         return 1;
     }
 
+    //Creates the buffer that will store the read bytes
     BYTE *buffer = malloc(512 * (sizeof(BYTE)));
     char newFiles[8];
     int JPEG_counter = 0;
 
-
+    //Checks if malloc worked
     if (buffer == NULL)
     {
         printf("malloc has failed \n");
@@ -25,24 +27,26 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-
+    //Opens the file and creates a file that will store the images
     FILE *raw = fopen(argv[1], "r");
     FILE *current;
 
     bool found = false;
 
     //printf("File opened!\n");
-
+    //Enters a loop that checks when there are enough bytes in the buffer
     while (fread(buffer, 1, 512, raw) == 512)
     {
-
+        //Condition for a new image file
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             //printf("Image \n");
-            if(found){
+            if (found)
+            {
                 fclose(current);
             }
 
+            //Handles the making of the new file name
             sprintf(newFiles, "%03i.jpg", JPEG_counter);
 
             JPEG_counter++;
@@ -51,17 +55,18 @@ int main(int argc, char *argv[])
             //printf("Made file \n");
 
             found = true;
-            }
-
-            if(found){
-                //printf("Wrote \n");
-                fwrite (buffer, 1, 512, current);
-            }
-
         }
 
+        if (found)
+        {
+            //printf("Wrote \n");
+            fwrite(buffer, 1, 512, current);
+        }
 
-    printf("Exit \n");
+    }
+
+
+    //printf("Exit \n");
 
     free(buffer);
     fclose(current);
