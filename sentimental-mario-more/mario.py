5e3b0bb03bec1ac69d8cb537f194b7d44f44db13
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-07-25 22:54:53 +0000

    Wed, Jul 26, 2023, 1:54 AM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,5 +1,12 @@
 from cs50 import get_int
 
-h = get_int
+while True:
+    h = get_int("Height: ")
 
-if h < 8
+    if h < 9 and h > 0:
+        break
+
+
+for i in range(h+1):
+    print(' ' * (h - i) + '#' * i + '  ' + '#' * i + ' ' * (h -i))
+
