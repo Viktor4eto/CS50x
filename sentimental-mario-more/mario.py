0719bd593ebd967a25057890de9a356a41028fd3
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-07-25 22:58:59 +0000

    Wed, Jul 26, 2023, 1:58 AM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,12 +1,13 @@
 from cs50 import get_int
 
+# Handles user input
 while True:
     h = get_int("Height: ")
 
     if h < 9 and h > 0:
         break
 
-
+# Prints out the pyramids
 for i in range(1, h+1):
     print(' ' * (h - i) + '#' * i + '  ' + '#' * i)
 
