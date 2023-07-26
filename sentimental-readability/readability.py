4516094c7f0d5f0927ebb2b49bd3bb82cc98efa3
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-07-26 23:04:34 +0000

    Thu, Jul 27, 2023, 2:04 AM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,6 +2,9 @@
 
 def main():
     text = input('Text: ')
-    letters, words, sentences = count_letters(text)
-    words = count_words(text)
-    sentences = count_sentences(text)
+    letters, words, sentences = analyze(text)
+
+
+def analyze(text):
+    for i in range(len(text)):
+        
