3835faefb0efc4cc8322c8626af9cb56c95087e1
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-07-26 23:09:51 +0000

    Thu, Jul 27, 2023, 2:09 AM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,8 +3,22 @@
 def main():
     text = input('Text: ')
     letters, words, sentences = analyze(text)
+    L = letters / words
+    S = sentences / words
 
 
 def analyze(text):
+    letters_count = 0
+    words_count = 0
+    sentences_count = 0
+
     for i in range(len(text)):
-        
+        if text[i].isalpha():
+            letters_count += 1
+        elif text[i] == ' ':
+            words_count += 1
+        elif text[i] == '.' or text[i] == '?' or text[i] == '!':
+            sentences_count += 1
+
+    return letters_count, words_count, sentences_count
+
