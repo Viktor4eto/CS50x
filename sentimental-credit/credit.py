371df8cffcdd5b4f6b9e535e038cf16909f36823
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-07-26 19:35:05 +0000

    Wed, Jul 26, 2023, 10:35 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,10 +1,14 @@
-
+import math
 
 def main():
     card = int(input('Number: '))
-    
+
 
     if legit(card):
-        if card / 10 ** 13 == 34 or card / 10 ** 13 == 37:
-            print('AMEX')
-        else if card / 10 ** 51 or
+        testVals = []
+        testVals[0] = math.floor(card / 10**13)
+        testVals[1] = math.floor(card / 10**14)
+        testVals[2] = math.floor(card / 10**12)
+        testVals[3] = math.floor(card / 10**15)
+
+        if 
