c8125c29b0bfed69bfdeb03be75b83af095c279d
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-07-26 23:12:10 +0000

    Thu, Jul 27, 2023, 2:12 AM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -6,6 +6,15 @@ def main():
     L = letters / words
     S = sentences / words
 
+    index = 0.0588 * L * 100 - 0.296 * S * 100 - 15.8
+
+    if index < 1:
+        print('Before Grade 1')
+    elif index > 16:
+        print('Grade 16+')
+    else:
+        print('Grade ' + round)
+
 
 def analyze(text):
     letters_count = 0
