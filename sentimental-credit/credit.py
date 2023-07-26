a8518f5aaca6cf03089d02979a6e3fca3074c484
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-07-26 19:31:31 +0000

    Wed, Jul 26, 2023, 10:31 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,8 +2,9 @@
 
 def main():
     card = int(input('Number: '))
+    
 
     if legit(card):
         if card / 10 ** 13 == 34 or card / 10 ** 13 == 37:
             print('AMEX')
-        else if card / 10 ** 14
+        else if card / 10 ** 51 or
