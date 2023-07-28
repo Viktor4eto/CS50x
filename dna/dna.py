56425138314d94ab35615165eed9cce2867c80a9
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-07-28 21:11:31 +0000

    Sat, Jul 29, 2023, 12:11 AM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,7 +5,9 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if sys.argv == 
+    if len(sys.argv) < 3:
+        print('Usage: python dna.py data.csv sequence.txt')
+        sys.exit()
 
     # TODO: Read database file into a variable
 
