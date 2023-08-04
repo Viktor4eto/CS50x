6c3cda2126c188ae0dc49306c69738d758fd995c
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-04 21:42:09 +0000

    Sat, Aug 5, 2023, 12:42 AM +03:00

---
+++ movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies JOIN people, stars, ratings ON people.id = stars.person_id AND stars.movie_id = movies.id AND movies.id = ratings.movie_id WHERE people.name = 'Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
