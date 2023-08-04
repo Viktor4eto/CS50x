31f0160d2ae8b4a4b82ed8649e38b1898b322ced
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-04 20:45:25 +0000

    Fri, Aug 4, 2023, 11:45 PM +03:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = 2010 ORDER BY rating, title;
+SELECT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE year = 2010 ORDER BY rating, title DESC;
