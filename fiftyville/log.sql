f07a6e19384c74c0e293f7bfe31fdfa91bdd692a
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-05 14:01:14 +0000

    Sat, Aug 5, 2023, 5:01 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -22,3 +22,9 @@ SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 A
 
 -- Relevant license plates
 SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute < 30;
+
+SELECT name, transcript  FROM interviews WHERE year = 2021 AND month = 7 AND day >= 28;
+
+-- Relevant license plates after interviews
+SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute < 25 AND activity =
+'exit';
