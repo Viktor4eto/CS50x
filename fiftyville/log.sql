d76c131f11e975f73896d778ab6722f1e3aa67ea
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-05 14:06:27 +0000

    Sat, Aug 5, 2023, 5:06 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -31,5 +31,5 @@ SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 A
 
 SELECT * FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
 
--- Table with callers and receivers around that time that lasted less than a minute
+-- Table with callers and receivers around that time that lasted less than a minute (The caller is the theif and the receiver is the accomplice)
 SELECT caller, receiver  FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
