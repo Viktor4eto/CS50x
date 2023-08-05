373d4735ea37e6bf3216a6f5679b010f4424984a
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-05 13:46:10 +0000

    Sat, Aug 5, 2023, 4:46 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,3 +11,7 @@ SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 A
 
 -- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
 -- Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
+
+SELECT * FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28;
+
+sqlite> SELECT * FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10;
