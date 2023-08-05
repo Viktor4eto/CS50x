929f19ba4e59d58f3485a68882cdb5407d844103
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-05 13:38:22 +0000

    Sat, Aug 5, 2023, 4:38 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,3 +7,5 @@ SELECT * FROM crime_scene_reports;
 
 SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28;
 
+SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
+
