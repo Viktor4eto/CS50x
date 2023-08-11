6b3dd036092d0e907dd1e0487ebd29444f6c7f2c
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-11 11:50:29 +0000

    Fri, Aug 11, 2023, 2:50 PM +03:00

--- homepage/script.js
+++ homepage/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
     let nav = document.getElementsByTagName("nav");
-    let buttons = nav.parentElement.children;
+    let buttons = nav[0].parentElement.childNodes;
     console.log(buttons);
 });
