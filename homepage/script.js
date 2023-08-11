cef315861f917ca3b7bbaf0ef34c957725d7e68a
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-11 12:25:49 +0000

    Fri, Aug 11, 2023, 3:25 PM +03:00

--- homepage/script.js
+++ homepage/script.js
@@ -4,9 +4,12 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for(button of buttons){
         button.addEventListener("click", function (){
-            let button_id = event.target.firstChild;
-            console.log(button_id)
-
+            let anchor_id = event.target.firstChild.id;
+            console.log(anchor_id)
+            var url = location.href;
+            console.log(url);              //Saving URL without hash.
+            location.href = "#"+anchor_id;                 //Navigate to the target element.
+            history.replaceState(null,null,url);
         });
     }
     console.log(buttons);
