0daaaeed569ec72d844b61edd094898e0a54e477
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-11 12:23:28 +0000

    Fri, Aug 11, 2023, 3:23 PM +03:00

--- homepage/script.js
+++ homepage/script.js
@@ -4,10 +4,9 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for(button of buttons){
         button.addEventListener("click", function (){
-            let button_id = event.target.firstChild.id;
+            let button_id = event.target.firstChild;
             console.log(button_id)
-            var get = document.getElementById(button_id);
-            console.log(get);
+
         });
     }
     console.log(buttons);
