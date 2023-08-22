4903f1603400305efe2893fba8989bdb058bdfe8
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-22 22:34:54 +0000

    Wed, Aug 23, 2023, 1:34 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -216,5 +216,7 @@ def sell():
         if ammount > db.execute("SELECT total FROM owned_shares WHERE user_id = ?", session["user_id"])[0]["total"]:
             return apology("You have less stocks than that", 400)
 
-        db.execute("")
+        db.execute("UPDATE owned_shares SET total = total - ? WHERE user_id = ? AND symbol = ?", ammount, session["user_id"], symbol)
+        db.execute("INSERT INTO purchases (user_id, symbol, shares) VALUES(?, ?, ?)", session["user_id"], symbol, -ammount)
+        
         return redirect("/")
