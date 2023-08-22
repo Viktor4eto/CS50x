7dcc8ebe226c0e3775442a25bb05b533d97eeada
Viktor4eto <100050049+Viktor4eto@users.noreply.github.com>
2023-08-22 23:12:36 +0000

    Wed, Aug 23, 2023, 2:12 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -223,7 +223,6 @@ def sell():
         db.execute("UPDATE owned_shares SET total = total - ? WHERE user_id = ? AND symbol = ?", ammount, session["user_id"], symbol)
         db.execute("INSERT INTO purchases (user_id, symbol, shares, price) VALUES(?, ?, ?, ?)", session["user_id"], symbol, -ammount, current_price)
         db.execute("UPDATE users SET cash = cash + ? WHERE id = ?", current_price*ammount, session["user_id"])
+        db.execute("DELETE FROM owned_shares WHERE user_id = ? AND total = 0", session[user_id])
 
-        if db.execute("SELECT symbol FROM owned_shares WHERE user_id = ? AND total = 0", session[user_id]):
-            
         return redirect("/")
